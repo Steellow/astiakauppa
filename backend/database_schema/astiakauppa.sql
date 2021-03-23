8d142f08e48b4cf0f30de68ac24835536b108784
Aleksi Ervasti <n0eral00@students.oamk.fi>
2021-03-23 13:15:40 +0200

    valmis tietokanta ja admin käyttäjä

--- backend/database_schema/astiakauppa.sql
+++ backend/database_schema/astiakauppa.sql
@@ -9,11 +9,17 @@ create database astiakauppa;
 use astiakauppa;
 
 create table users (
-    userid integer primary key auto_increment,
+    id integer primary key auto_increment,
     username char(50) unique not null,
+    firstname char(50) not null,
+    lastname char(50) not null,
     password char(50) not null,
-    admin smallint default 0,
-    email char(100) not null
+    email char(100) not null,
+    address char(100) not null,
+    city char(50) not null,
+    postalcode integer,
+    admin smallint default 0
+    
 );
 
 create table productgroup (
@@ -21,24 +27,33 @@ create table productgroup (
     name char(50) not null
 );
 
-create table products (
+create table product (
     id integer primary key auto_increment,
     name char(50) not null,
+    cost decimal(5,2),
     price decimal(5,2),
     groupid smallint not null,
-    pic char(255)
-    foreign key(groupid) references productgroup(groupid) on delete cascade
+    pic char(255),
+    description char(255),
+    foreign key(groupid) references productgroup(id) on delete cascade
 );
 
-create table order (
-    ordernum integer,
-    userid integer,
+create table orders (
+    ordernum integer primary key auto_increment,
+    userid integer not null,
     orderdate date,
+    status char(20),
+    foreign key(userid) references users(id)
 );
 
-create table orderrow (
+create table order_row (
     ordernum integer,
     rownum smallint,
-    pid integer,
-    amount integer
+    productid integer,
+    amount integer,
+    primary key(ordernum,rownum)
 );
+
+-- verkkokaupan omistaja
+insert into users(username,firstname,lastname,password,email,address,city,postalcode,admin)
+values('admin', 'Jorma','Jermula','admin123','jormajermu@hotmail.com','jormankatu 12', 'Oulu', 90150, 1);
