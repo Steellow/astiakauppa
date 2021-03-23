e7252e4a49f52c0c16cc659311ae8064f0b4f821
Steellow <hankilapio@gmail.com>
2021-03-23 19:03:14 +0200

    Korjailin erroreita footterista

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,26 +1,32 @@
 import React from "react";
-import styles from '../styles/styles.css';
+// 👇👇👇 react valitti jotain turhaa niin disablasin sen varotuksen t. hanki
+// eslint-disable-next-line
+import styles from "../styles/styles.css";
 
 export default function Footer() {
   return (
     <footer className="footer row bg-dark">
-      <p className="col-sm-12, col-md-3 text-white">
-          <ul class="footer_list">
-            <li class="footer_list_fi">Ota yhteyttä</li>
-            <li>puhelinnumero</li>
-            <li>sähköpostiosoite</li>
-            <li>osoite</li>
-          </ul>
-      </p>
+      <div className="col-sm-12, col-md-3 text-white">
+        <ul className="footer_list">
+          <li className="footer_list_fi">Ota yhteyttä</li>
+          <li>puhelinnumero</li>
+          <li>sähköpostiosoite</li>
+          <li>osoite</li>
+        </ul>
+      </div>
       <p className="col-sm-12, col-md-3 text-white">
         Seuraa meitä somessa <br />
-        <a href="https://fi-fi.facebook.com/" target="1" class="fa fa-facebook"></a>
-        <a href="https://www.instagram.com/" target="2" class="fa fa-instagram"></a>
-        <a href="https://twitter.com/?lang=fi" target="3" class="fa fa-twitter"></a>
+        <a href="https://fi-fi.facebook.com/" target="1">
+          <i className="fa fa-facebook"></i>
+        </a>
+        <a href="https://www.instagram.com/" target="2">
+          <i className="fa fa-instagram"></i>
+        </a>
+        <a href="https://twitter.com/?lang=fi" target="3">
+          <i className="fa fa-twitter"></i>
+        </a>
       </p>
-      <p className="col-sm-12, col-md-6 text-white">
-        Tekstiä...
-      </p>
+      <p className="col-sm-12, col-md-6 text-white">Tekstiä...</p>
       <p className="col-12 text-center bg-secondary text-white">
         Hannes Kinnunen | Aleksi Ervasti | Milla Viitapohja | Sanni Ristimella | Arttu Heikkinen
       </p>
