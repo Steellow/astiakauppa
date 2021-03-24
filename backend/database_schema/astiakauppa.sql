5f9ce426b982a37873e1058d01dee81b296796ba
n0vimi00 <n0vimi00@students.oamk.fi>
2021-03-24 13:45:47 +0200

    muumituotteita

--- backend/database_schema/astiakauppa.sql
+++ backend/database_schema/astiakauppa.sql
@@ -68,3 +68,70 @@ values ('kupit');
 
 insert into product (name,cost,price,groupid,pic,description)
  VALUES ('kuppi',10.50,15.50,1,'kuvalinkki','vihreä muki syvä ku mikä');
+
+ -- tuoteryhmät
+
+insert into productgroup(name)
+values('Lautaset');
+
+insert into productgroup(name)
+values('Mukit');
+
+insert into productgroup(name)
+values('Lautaset');
+
+--TUOTTEET
+
+-- mukit
+
+insert into product(name,cost,price,groupid,pic,description)
+values('Muumimuki: Nuuskamuikkunen vihreä', 5,12.90,2,'https://arabia.fi/var/rorstrand/storage/images/frontpage/astiasarjat/muumi-muki-listaus/2015-nuuskamuikkunen-vihrea/listaus-listing/26865-1-fin-FI/listaus-listing.png',
+'Keväisen vihreä Muumimuki. Tilavuus 0,3 l. Konepesun kestävä.');
+
+insert into product(name,cost,price,groupid,pic,description)
+values('Muumimuki: Pikkumyy punainen', 5,9.90,2,'https://arabia.fi/var/rorstrand/storage/images/frontpage/astiasarjat/muumi-muki-listaus/2015-pikku-myy-punainen/listaus-listing/26937-1-fin-FI/listaus-listing.png',
+'Pirteän punainen Muumimuki! Tilavuus 0,3 l. Konepesun kestävä.');
+
+insert into product(name,cost,price,groupid,pic,description)
+values('Muumimuki: Nipsu turkoosi', 5,16.90,2,'https://arabia.fi/var/rorstrand/storage/images/frontpage/astiasarjat/muumi-muki-listaus/2008-nipsu-turkoosi/listaus-listing/26076-1-fin-FI/listaus-listing.png',
+'Raikkaan turkoosissa Muumimukissa seikkailee rikkauksista haaveileva hassunhauska Nipsu. Tilavuus 0,3 l. Konepesun kestävä.');
+
+insert into product(name,cost,price,groupid,pic,description)
+values('Muumimuki: Mymmeli', 5,14.90,2,'https://arabia.fi/var/rorstrand/storage/images/frontpage/astiasarjat/muumi-muki-listaus/2008-mymmeli/listaus-listing/26148-1-fin-FI/listaus-listing.png',
+'Pirteän punakassa Arabian Muumimukissa istuu ihana ja huoleton Mymmeli. Tilavuus 0,3 l. Konepesun kestävä.');
+
+-- lautaset
+
+insert into product(name,cost,price,groupid,pic,description)
+values('Muumilautanen: Nuuskamuikkunen vihreä', 7,21.90,1,'https://iittala.scene7.com/is/image/iittala/?qlt=90&op_usm=1.2,1,0,0&src=is{iittala/arabia-moomin-plate-19_cm-snufkin?size=3800,3800&wid=2000&hei=2000&scl=2.0}&extend=100,100,100,100',
+'Huuliharppua soittava Nuuskamuikkunen on lempeä ja huoleton kulkuri. ⌀19cm Konepesun kestävä.');
+
+insert into product(name,cost,price,groupid,pic,description)
+values('Muumilautanen: Pikkumyy punainen', 7,18.90,1,'https://iittala.scene7.com/is/image/iittala/?qlt=90&op_usm=1.2,1,0,0&src=is{iittala/arabia-moomin-plate-19_cm-little-my?size=3800,3800&wid=2000&hei=2000&scl=2.0}&extend=100,100,100,100',
+'Pikku Myy on Mymmelin lapsista kaikista pienin ja pippurisin. ⌀19cm Konepesun kestävä.');
+
+insert into product(name,cost,price,groupid,pic,description)
+values('Muumilautanen: syvä Nipsu turkoosi', 7,23.90,1,'https://iittala.scene7.com/is/image/iittala/?qlt=90&op_usm=1.2,1,0,0&src=is{iittala/arabia-moomin-bowl-15_cm-sniff?size=3800,3800&wid=2000&hei=2000&scl=2.4}&extend=100,100,100,100',
+'Luonteeltaan kiltti, mutta hieman pelokaskin Nipsu rakastaa kallisarvoisia, kiiltäviä ja kimaltavia esineitä. ⌀15cm Konepesun kestävä.');
+
+insert into product(name,cost,price,groupid,pic,description)
+values('Muumilautanen: syvä Mymmeli', 7,23.90,1,'https://iittala.scene7.com/is/image/iittala/?qlt=90&op_usm=1.2,1,0,0&src=is{iittala/arabia-moomin-bowl-15_cm-mymble?size=3800,3800&wid=2000&hei=2000&scl=2.0}&extend=100,100,100,100',
+'Mymmeli unelmoi mielellään elämänsä rakkauden löytämisestä. Hän ihastuu usein ja haaveilee erityisesti Poliisimestarista. ⌀15cm Konepesun kestävä.');
+
+-- lasit
+
+insert into product(name,cost,price,groupid,pic,description)
+values('Muumilasi: Muumipappa', 3,11.90,3,'https://iittala.scene7.com/is/image/iittala/?&src=is{iittala/arabia%2Dmoomin%2Dtumbler%2D22cl%2Dmoominpappa1?size=3800,3800&wid=2000&hei=2000&scl=2.4}&extend=100,100,100,100',
+'Muumipappa onkii kaikessa rauhassa. Tilavuus 22 cl. Konepesun kestävä.');
+
+insert into product(name,cost,price,groupid,pic,description)
+values('Muumilasi: Pikkumyy', 3,12.90,3,'https://iittala.scene7.com/is/image/iittala/?&src=is{iittala/arabia%2Dmoomin%2Dtumbler%2D22cl%2Dlittle%5Fmy?size=3800,3800&wid=2000&hei=2000&scl=2.4}&extend=100,100,100,100',
+'Pikku Myy kelluu lumpeiden kanssa niin, että vain nuttura näkyy. Tilavuus 22 cl. Konepesun kestävä.');
+
+insert into product(name,cost,price,groupid,pic,description)
+values('Muumilasi: Niiskuneiti', 3,12.90,3,'https://iittala.scene7.com/is/image/iittala/?&src=is{iittala/arabia%2Dmoomin%2Dtumbler%2D22cl%2Dsnorkmaiden1?size=3800,3800&wid=2000&hei=2000&scl=2.4}&extend=100,100,100,100',
+'Niiskuneiti on lähtenyt soutelemaan. Tilavuus 22 cl. Konepesun kestävä.');
+
+insert into product(name,cost,price,groupid,pic,description)
+values('Muumilasi: Muumipeikko', 3,12.90,3,'https://iittala.scene7.com/is/image/iittala/?&src=is{iittala/arabia%2Dmoomin%2Dtumbler%2D22cl%2Dmoomintroll1?size=3800,3800&wid=2000&hei=2000&scl=2.4}&extend=100,100,100,100',
+'Muumipeikko sukeltelee uteliaana vedessä. Tilavuus 22 cl. Konepesun kestävä.');
