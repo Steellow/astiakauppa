ea1faa0b4c741a82c6a7997285f7b24d99dbfde7
Steellow <hankilapio@gmail.com>
2021-03-26 11:10:25 +0200

    create file to retrieve single item

---
+++ backend/astiakauppa_tietokanta/getitem.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'inc/functions.php';
+require_once 'inc/headers.php';
+
+try {
+    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+    $conn = openDb();
+    $stmt = $conn->prepare("SELECT * FROM product WHERE id = :id");
+    $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $results = $stmt->fetchAll();
+
+    header('HTTP/1.1 200 OK');
+    echo json_encode($results);
+} catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
