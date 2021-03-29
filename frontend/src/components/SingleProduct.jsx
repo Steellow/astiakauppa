f2d154ea4c59f7f00b076e323fd1e99dc5e0213b
Steellow <hankilapio@gmail.com>
2021-03-29 13:47:17 +0300

    Remove unecessary comment

--- frontend/src/components/SingleProduct.jsx
+++ frontend/src/components/SingleProduct.jsx
@@ -5,7 +5,6 @@ export default function SingleProduct({ product }) {
   return (
     <div className="col-12 col-sm-6 col-md-4 col-lg-3 my-2">
       <Link to={`/tuote/${product.id}`} style={{ color: "inherit", textDecoration: "inherit" }}>
-        {/* TODO: ☝☝☝ Laita linkkiin tuotteen ID äläkä title */}
         <div className="card h-100">
           <img className="card-img-top p-2" src={product.pic} alt="Tuotekuva" />
           <div className="card-body pb-1 d-flex flex-column justify-content-between">
