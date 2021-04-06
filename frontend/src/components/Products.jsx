9a970c752728628641360e06b5bfff862cb7ccd8
Steellow <hankilapio@gmail.com>
2021-04-06 11:32:48 +0300

    == to ===

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -3,16 +3,17 @@ import SingleProduct from "./SingleProduct";
 import NoResultsPage from "../pages/NoResultsPage";
 
 export default function Products({ products }) {
-  if (products===undefined || products.length == 0) {
+  if (products === undefined || products.length === 0) {
     return <NoResultsPage />;
   } else {
-  return (
-    <div className="container">
-      <div className="row">
-        {products.map((product) => {
-          return <SingleProduct key={product.id} product={product} />;
-        })}
+    return (
+      <div className="container">
+        <div className="row">
+          {products.map((product) => {
+            return <SingleProduct key={product.id} product={product} />;
+          })}
+        </div>
       </div>
-    </div>
-  );}
+    );
+  }
 }
