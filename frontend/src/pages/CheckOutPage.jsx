43a23ba12068eaf51f404dff6be1f17bfa1649ae
n0vimi00 <n0vimi00@students.oamk.fi>
2021-04-07 15:57:26 +0300

    checkout.php

--- frontend/src/pages/CheckOutPage.jsx
+++ frontend/src/pages/CheckOutPage.jsx
@@ -36,91 +36,91 @@ export default function CheckOutPage(item) {
                   <h4>Asiakastiedot</h4>
                <div>
     
-              <form>
-                <div class="form-group">
-                        <label for="fullname"><i class="fa fa-user"></i> Koko nimi</label>
-                  <div class="row">
-                    <div class="col-md-6">
-                        <input type="text" class="form-control" id="inputFname" placeholder="Etunimi"/>
+              <form action="checkout.php" method="get">
+                <div className="form-group">
+                        <label for="fullname"><i className="fa fa-user"></i> Koko nimi</label>
+                  <div className="row">
+                    <div className="col-md-6">
+                        <input type="text" className="form-control" id="firstname" placeholder="Etunimi"/>
                     </div>
-                    <div class="form-group col-md-6">
-                        <input type="text" class="form-control" id="inpuLname" placeholder="Sukunimi"/>
+                    <div className="form-group col-md-6">
+                        <input type="text" className="form-control" id="lastname" placeholder="Sukunimi"/>
                     </div>
                   </div>
                 </div>
     
-                  <div class="form-group">
-                      <div class="form-group">
-                        <label for="inputEmail"><i class="fa fa-envelope"></i> Sähköposti</label>
-                        <input type="email" class="form-control" id="inputEmail" placeholder="Sähköpostiosoite"/>
+                  <div className="form-group">
+                      <div className="form-group">
+                        <label for="email"><i className="fa fa-envelope"></i> Sähköposti</label>
+                        <input type="email" className="form-control" id="email" placeholder="Sähköpostiosoite"/>
                       </div>
                   </div>
     
-                  <div class="form-group ">
-                      <label for="inputAddress"><i class="fa fa-address-card-o"></i> Osoite</label>
-                      <input type="text" class="form-control" id="inputAddress" placeholder="Katuosoite"/>
+                  <div className="form-group ">
+                      <label for="address"><i className="fa fa-address-card-o"></i> Osoite</label>
+                      <input type="text" className="form-control" id="address" placeholder="Katuosoite"/>
                   </div>
     
-                  <div class="row">
-                  <div class="col-md-6 mb-3">
-                      <label for="inputCity">Kaupunki</label>
-                      <input type="text" class="form-control" id="inputCity"/>
+                  <div className="row">
+                  <div className="col-md-6 mb-3">
+                      <label for="city">Kaupunki</label>
+                      <input type="text" className="form-control" id="city"/>
                   </div>
-                  <div class="form-group col-md-5">
-                      <label for="inputZip">Postinumero</label>
-                      <input type="text" class="form-control" id="inputZip"/>
+                  <div className="form-group col-md-5">
+                      <label for="zip">Postinumero</label>
+                      <input type="text" className="form-control" id="zip"/>
                   </div>
                 
-                    <div class="form-group">
-                      <div class="custom-control custom-checkbox">
-                          <input type="checkbox" class="custom-control-input" id="same-address"/>
-                          <label class="custom-control-label" for="same-address">Toimitusosoitteeni on sama kuin laskutus.</label>
+                    <div className="form-group">
+                      <div className="custom-control custom-checkbox">
+                          <input type="checkbox" className="custom-control-input" id="same-address"/>
+                          <label className="custom-control-label" for="same-address">Toimitusosoitteeni on sama kuin laskutus.</label>
                       </div>
                     </div>
     
                   {/* Maksutapa */}
     
-                  <div class="form-group">
+                  <div className="form-group">
                     <h4>Maksutapa</h4>
     
-                    <div class="form-group">
-                        <div class="custom-control custom-radio">
-                            <input id="credit" name="paymentMethod" type="radio" class="custom-control-input"  required=""/>
-                            <label class="custom-control-label" for="credit">Maksukortti</label>
+                    <div className="form-group">
+                        <div className="custom-control custom-radio">
+                            <input id="credit" name="paymentMethod" type="radio" className="custom-control-input"  required=""/>
+                            <label className="custom-control-label" for="credit">Maksukortti</label>
                         </div>
-                        <div class="custom-control custom-radio">
-                            <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required=""/>
-                            <label class="custom-control-label" for="paypal">PayPal</label>
+                        <div className="custom-control custom-radio">
+                            <input id="paypal" name="paymentMethod" type="radio" className="custom-control-input" required=""/>
+                            <label className="custom-control-label" for="paypal">PayPal</label>
                         </div>
                     </div>
     
-                      <div class="form-group">
+                      <div className="form-group">
                           <label for="cname">Nimi kortilla</label>
-                          <input type="text" class="form-control" id="cname" name="cardname" placeholder="Maija Mehiläinen" required=""/>
-                          <small class="text-muted">Koko nimi kuten kortilla näkyy</small>
-                            <div class="invalid-feedback"> Kortin nimi vaaditaan </div>
+                          <input type="text" className="form-control" id="cname" name="cardname" placeholder="Maija Mehiläinen" required=""/>
+                          <small className="text-muted">Koko nimi kuten kortilla näkyy</small>
+                            <div className="invalid-feedback"> Kortin nimi vaaditaan </div>
                       </div>   
-                      <div class="form-group"> 
+                      <div className="form-group"> 
                           <label for="ccnum">Kortin numero</label>
-                          <input type="text" class="form-control" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444" required=""></input>
-                          <div class="invalid-feedback"> Luottokortin numero vaaditaan </div>
+                          <input type="text" className="form-control" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444" required=""></input>
+                          <div className="invalid-feedback"> Luottokortin numero vaaditaan </div>
                      </div>
     
-                    <div class="form-row">
-                    <div class="col-md-3 mb-3">
+                    <div className="form-row">
+                    <div className="col-md-3 mb-3">
                         <label for="expyyear">Vanhenee</label>
-                        <input type="text" class="form-control" id="expyear" name="expyear" placeholder="KK/VV" required=""></input>
-                        <div class="invalid-feedback"> Expiration date required </div>
+                        <input type="text" className="form-control" id="expyear" name="expyear" placeholder="KK/VV" required=""></input>
+                        <div className="invalid-feedback"> Expiration date required </div>
                     </div>
-                    <div class=" col-md-3">
+                    <div className=" col-md-3">
                     <label for="cvv">CVV</label>
-                     <input type="text" class="form-control" id="cvv" name="cvv" placeholder="352" required=""></input>
-                     <div class="invalid-feedback"> Security code required </div>
+                     <input type="text" className="form-control" id="cvv" name="cvv" placeholder="352" required=""></input>
+                     <div className="invalid-feedback"> Security code required </div>
                     </div>
                   </div>
                   </div>
-                  <hr class="mb-4"/>
-                    <button class="btn btn-success btn-lg btn-block" type="submit">Maksa tilaus</button>
+                  <hr className="mb-4"/>
+                    <button className="btn btn-success btn-lg btn-block" type="submit">Maksa tilaus</button>
                   </div>        
                 </form>
                 </div>
@@ -128,10 +128,10 @@ export default function CheckOutPage(item) {
               
                {/* Ostoskori  */}
                <div className="col-12 col-md-5 p-4 ml-5 mt-2 mb-5">
-                  <h4 class="d-flex justify-content-between align-items-center mb-3">
-                    <span >Ostoskori <i class="fa fa-shopping-cart"></i></span>
+                  <h4 className="d-flex justify-content-between align-items-center mb-3">
+                    <span >Ostoskori <i className="fa fa-shopping-cart"></i></span>
                   </h4>
-                <ul class="list-group mb-3 sticky-top">
+                <ul className="list-group mb-3 sticky-top">
                   <div>
                     {items.map((item) => {
                       return (
@@ -139,7 +139,7 @@ export default function CheckOutPage(item) {
                       );
                     })}
                   </div>
-                  <li class="list-group-item d-flex justify-content-between">
+                  <li className="list-group-item d-flex justify-content-between">
                     <span>Yhteensä</span>
                     <strong>{total.toFixed(2)}€</strong>
                   </li>
