40ad48baa16718475bf7c47c737859973c5bb131
n0vimi00 <n0vimi00@students.oamk.fi>
2021-04-09 23:40:28 +0300

    checkout.php

--- backend/astiakauppa_tietokanta/checkout.php
+++ backend/astiakauppa_tietokanta/checkout.php
@@ -35,6 +35,14 @@ try {
   $order->bindValue(2, $curdate);
   $order->execute();
 
+   // ORDER_ROW-TAULU 
+
+   $ordernum = $db->lastInsertId(); 
+
+   $orderrow = $db->prepare("INSERT INTO order_row (ordernum) SELECT ordernum from orders WHERE `ordernum`=?");
+   $orderrow->bindValue(1, $ordernum);
+   $orderrow->execute();
+
   header('HTTP/1.1 200 OK');
   Header("Location: http://localhost:3000/"); /* oma sivu onnistuneelle checkoutille? */
   
