0cd19e877a34b350d1507b544a0a8f1a74f90c22
ArttuHeikkine <n0hear00@students.oamk.fi>
2021-04-13 01:53:43 +0300

    Update Products.jsx

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -3,35 +3,18 @@ import SingleProduct from "./SingleProduct";
 
 export default function Products({ products }) {
   const [data, setData] = useState([]);
-  const [sortType, setSortType] = useState('1');
-
-  useEffect(() => {
-    const sortArray = type => {
-      const types = {
-        1: '1',
-        2: '2',
-        3: '3',
-      };
-      const sortProperty = types[type];
-      const sorted = [...products].sort((a, b) => b[sortProperty] - a[sortProperty]);
-      setData(sorted);
-    };
-
-    sortArray(sortType);
-  }, [sortType]);
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-12">
 
-      <select onChange={(e) => setSortType(e.target.value)} 
-      style={{ marginTop: "10px", marginBottom: "10px" }} className="btn btn-outline-dark">
+      <select style={{ marginTop: "10px", marginBottom: "10px" }} className="btn btn-outline-dark">
         <option value="1">Lajittele</option>
         <option value="2">Halvin ensin</option>
         <option value="3">Kallein ensin</option>
+        <option value="4">Aakkosjärjestyksessä</option>
       </select>
-
         </div>
         
         {products.map((product) => {
